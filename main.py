a092b4ae6e2285f195b5d1c3d6fa6ebc3dbc2fb2
Paolo Anzani <69012431+yungpanz@users.noreply.github.com>
2021-10-07 14:40:44 +0200

    outputs the total size of removed files

--- main.py
+++ main.py
@@ -36,34 +36,34 @@ folders_full = Folder_list(reader(args.config, "folders_full"))
 
 # Delete a given folder
 
-def clean_folder(folder: str):
+def clean_folder(folder: str) -> int:
     folder = folder.replace("~", root_home)
+    bytes_count = 0
     # List all the content of a dir
     for file_name in os.listdir(folder):
         file_path = os.path.join(folder, file_name)
         # Check if file or directory
         if os.path.isfile(file_path) or os.path.islink(file_path):
-            log_size(file_path)
+            bytes_count += log_size(file_path)
             subprocess.call(['rm', file_path])
         elif os.path.isdir(file_path):
-            log_size(file_path)
+            bytes_count += log_size(file_path)
             shutil.rmtree(file_path)
+    return bytes_count
             
 # Main function
 
 def main():
+    removed_size = 0
     for f in folders:
-        clean_folder(f)
+        removed_size += clean_folder(f)
     
     # if --full is passed execute full clean
     if args.full:
         for f in folders_full:
-            clean_folder(f)
-        # system reboot is necessary when deleting /private/var/folders 
-        subprocess.call(['sudo', 'reboot'])
+            removed_size += clean_folder(f)
 
-    print("Cleaning complete")
-
+    print(f'Total space cleaned: {removed_size} bytes')
 
 if __name__ == "__main__":
 
