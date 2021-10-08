e56a1981d37ebfe5a554c1c425ac3507639d633b
Paolo Anzani <69012431+yungpanz@users.noreply.github.com>
2021-10-08 09:27:35 +0200

    add http server to host the UI

---
+++ server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Paolo Anzani <p.anzani@campus.unimib.it> 10-07-2021
+
+# Serve UI app on localhost
+
+from flask import Flask, request
+import os, subprocess, json
+
+# Flask settings
+app = Flask(__name__, static_url_path="/ui", static_folder='./ui')
+
+# Load html files
+index = open("ui/index.html", "r").read()
+
+@app.route("/")
+def main():
+    return index
+
+# Dialog GUI activation end-point
+@app.route("/dialog", methods=['POST'])
+def dialog():
+    if request.method == 'POST':
+        path = str(subprocess.check_output(['python3', 'dialog.py']))
+        response = json.dumps({'data': path[2:-3], 'status': 'succes', 'code': 201})
+        print(path) # Log it in the console
+        return response
+    else:
+        return 'wrong type request'
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+    
+
