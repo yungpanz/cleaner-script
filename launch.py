97134cea4efc6d7fcaeff1ff44e412b1cf23c3ed
Paolo Anzani <69012431+yungpanz@users.noreply.github.com>
2021-10-10 16:35:52 +0200

    launch the UI via cli

---
+++ launch.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Paolo Anzani <p.anzani@campus.unimib.it> 10-10-2021
+
+# Launch UI
+
+import webbrowser as wb
+import subprocess
+
+# Global variables
+url = 'http://localhost:5000/'
+
+def main():
+    # Open in a new window if not already open
+    wb.open_new(url)
+    # Start server
+    subprocess.run(["python3", "server.py"])
+
+    
+if __name__ == "__main__":
+    main()
